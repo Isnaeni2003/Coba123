db21c67ebe535b97f553cf3d2cf7ba7097cdad8e
root <bobwatcherx@gmail.com>
2022-10-23 10:06:14 +0700

    punya aku

---
+++ latihan.py
@@ -0,0 +1,13 @@
+a = input("masukkan nilai a = ")
+b = input("masukkan nilai b = ")
+
+print (" variable a = ",a)
+print (" variable b = ",b)
+
+
+a = int(a)
+b = int(b)
+
+print("hasil penjumlahan (1)+(0) = %d".format(a,b) %(a+b))
+print("hasil pembagian (1)/(0) = %d".format(a,b) %(a+b))
+print("Hasil penggabungan (1)&(0)=%d".format(a,b) %(a+b))
